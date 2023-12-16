a50b4b0ead4d7504324ab7592b07fe83825d5726
BratuAndreiEugen <andreieugenbratu@gmail.com>
2023-12-16 22:12:27 +0200

    added bean for validation class

--- baxbanii/src/main/java/org/example/validation/ValidateRating.java
+++ baxbanii/src/main/java/org/example/validation/ValidateRating.java
@@ -1,7 +1,11 @@
 package org.example.validation;
 
+import lombok.AllArgsConstructor;
 import org.example.data.entity.Rating;
+import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
+@Component
 public class ValidateRating {
     public void validateRating(Rating rating) throws Exception {
 
